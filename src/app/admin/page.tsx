59191c8fb22cdb8e87f838fa597147de5ed81c6c
Rahul Biswas <rahul114biswas@gmail.com>
2025-06-23 22:55:11 +0600

    Removed unused globals.css import to fix postcss build error

--- src/app/admin/page.tsx
+++
@@ -1,4 +0,0 @@
-// app/admin/page.tsx
-export default function AdminHomePage() {
-  return <div className="p-5 text-xl text-red-600">🛡️ অ্যাডমিন হোম পেজ</div>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div style={{ color: 'red', fontSize: '40px' }}>
+    <div>
       If you see this red text, rahul biswas raw CSS is working.
     </div>
   );
